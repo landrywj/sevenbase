bd7f4998e059c73c5fe7f512b3bbbbf47d5c6d7e
wilfrid.io <info@pipeline-e.com>
2023-08-21 20:46:54 -0700

    Add admin users view

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-
   # Defines the root path route ("/")
-  root "application#home"
+  root 'application#home'
   get 'application/home', to: 'application#home', as: 'home'
-  resources :rooms do 
+  resources :rooms do
     resources :messages
-  end 
+  end
   resources :blogs, :messages, :blahs
   devise_for :users
+  namespace :admin do
+    get 'users', to: 'users#index', as: 'users'
+    get 'users/show'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-
 end
